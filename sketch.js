43751f06b6a7895e14dcc6e077df74f5afbceac2
CCMSM2022B34 <118444019+CCMSM2022B34@users.noreply.github.com>
2022-11-17 09:12:50 -0300

    primeira fase do pong

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+ createCanvas(400, 400);
+}
+let xBol=200;
+let yBol=200;
+let diametro=50;
+let velocidadeX=1;
+function draw() {
+ background(0);
+ circle(xBol,yBol,diametro);
+ xBol +=velocidadeX;
+ if (xBol+diametro/2>=400){
+  velocidadeX*=-1;
+ } 
+ if (xBol-diametro/2<=0){
+    velocidadeX*=-1;
+ }
+rect(0,15,20.6);
+}
