47a51ffc03025d5de84ded3abfb9390a371e3249
CCMSM2022B34 <118444019+CCMSM2022B34@users.noreply.github.com>
2022-12-01 09:43:31 -0300

    aula 01/12.2

--- sketch.js
+++ sketch.js
@@ -20,10 +20,20 @@ function draw() {
  if (xBol+diametro/2>=400){
   velocidadeX*=-1;
  }
-  //faz a bolinha voltar quando bate na direita 
+  //faz a bolinha voltar quando bate na esquerda
  if (xBol-diametro/2<=0){
     velocidadeX*=-1;
  }
-rect(0,150,25,100);
+  //faz a bolinha quicar em cima e em baixo 
+  if(yBol-diametro/2<=0){
+  velocidadey*=-1;
+  }
+  //faz a bolinha quicar em cina e em baixo
+  if(yBol+diametro/2>=400){
+  
+    velocidadey*=-1
+    }
+rect(0,150,15,100);
+  
 }
 
