d78283e0117e883406d241f3495040e67af2a53e
CCMSM2022B34 <118444019+CCMSM2022B34@users.noreply.github.com>
2022-12-01 09:10:57 -0300

    aula de 01/12

--- sketch.js
+++ sketch.js
@@ -1,19 +1,29 @@
 function setup() {
  createCanvas(400, 400);
 }
+//variaveis da bolinha
 let xBol=200;
 let yBol=200;
 let diametro=50;
 let velocidadeX=1;
+let velocidadey=1;
+//variaveis da raquete
 function draw() {
  background(0);
+  //desenha a bolinha
+  
  circle(xBol,yBol,diametro);
+  //movimenta a bolinha 
  xBol +=velocidadeX;
+  yBol+=velocidadey;
+  //faz a bolinha voltar quanda bate na direita 
  if (xBol+diametro/2>=400){
   velocidadeX*=-1;
- } 
+ }
+  //faz a bolinha voltar quando bate na direita 
  if (xBol-diametro/2<=0){
     velocidadeX*=-1;
  }
-rect(0,15,20.6);
+rect(0,150,25,100);
 }
+
